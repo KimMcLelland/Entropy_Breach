03a5f9d8f2e2ee638f06f557af742880f233c55c
Kim <faeriekim3@gmail.com>
2025-02-01 11:32:22 +0000

    gravity and movement according to godot tutorial

--- Player.gd
+++ Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+var velocity = Vector2(0, 1)
 
 func _ready():
 	pass 
@@ -9,4 +12,14 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	move_and_collide(Vector2(0, 1))
+	velocity.y += delta * GRAVITY
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+		$AnimatedSprite.animation = "left"
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+		$AnimatedSprite.animation = "right"
+	else:
+		velocity.x = 0
+	
+	move_and_slide(velocity)
