2ba15a44329efb612f6eba8fb947ceeb36833495
Kim <faeriekim3@gmail.com>
2025-02-07 14:43:31 +0000

    sand recoloured, game environment bigger, camera and clamp on player

--- Player.gd
+++ Player.gd
@@ -25,5 +25,5 @@ func _physics_process(delta):
 		
 	#if Input.is_action_pressed("Jump") and is_on_floor():
 		#velocity.y -= JumpForce
-	
+	position.x = clamp(position.x, 32, 3040)
 	move_and_slide(velocity)
